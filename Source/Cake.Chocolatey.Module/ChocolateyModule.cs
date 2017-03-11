f1da018160aded4e18acdf22f2181448b918a052
Gary Ewan Park <gep13@gep13.co.uk>
2017-03-11 22:42:33 +0000

    Initial Commit

---
+++ Source/Cake.Chocolatey.Module/ChocolateyModule.cs
@@ -0,0 +1,27 @@
+﻿using Cake.Core.Composition;
+using Cake.Core.Packaging;
+using System;
+
+namespace Cake.Chocolatey.Module
+{
+    /// <summary>
+    /// The module responsible for registering
+    /// default types in the Cake.Chocolatey.Module assembly.
+    /// </summary>
+    public sealed class ChocolateyModule : ICakeModule
+    {
+        /// <summary>
+        /// Performs custom registrations in the provided registrar.
+        /// </summary>
+        /// <param name="registrar">The container registrar.</param>
+        public void Register(ICakeContainerRegistrar registrar)
+        {
+            if (registrar == null)
+            {
+                throw new ArgumentNullException(nameof(registrar));
+            }
+
+            registrar.RegisterType<ChocolateyPackageInstaller>().As<IPackageInstaller>().Singleton();
+        }
+    }
+}
---
+++ Source/Cake.Chocolatey.Module/ChocolateyPackageInstaller.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using Cake.Core.IO;
+using Cake.Core.Packaging;
+using Cake.Core;
+using Cake.Core.Diagnostics;
+using Cake.Core.Configuration;
+using System.Linq;
+
+namespace Cake.Chocolatey.Module
+{
+    /// <summary>
+    /// Installer for Chocolatey Packages.
+    /// </summary>
+    public sealed class ChocolateyPackageInstaller : IPackageInstaller
+    {
+        private readonly ICakeEnvironment _environment;
+        private readonly ICakeLog _log;
+        private readonly IProcessRunner _processRunner;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChocolateyPackageInstaller"/> class.
+        /// </summary>
+        /// <param name="environment">The environment.</param>
+        /// <param name="processRunner">The process runner.</param>
+        /// <param name="log">The log.</param>
+        public ChocolateyPackageInstaller(ICakeEnvironment environment, IProcessRunner processRunner, ICakeLog log)
+        {
+            if (environment == null)
+            {
+                throw new ArgumentNullException(nameof(environment));
+            }
+
+            if (processRunner == null)
+            {
+                throw new ArgumentNullException(nameof(processRunner));
+            }
+
+            if (log == null)
+            {
+                throw new ArgumentNullException(nameof(log));
+            }
+
+            _environment = environment;
+            _processRunner = processRunner;
+            _log = log;
+        }
+
+        /// <summary>
+        /// Determines whether this instance can install the specified resource.
+        /// </summary>
+        /// <param name="package">The package reference.</param>
+        /// <param name="type">The package type.</param>
+        /// <returns><c>true</c> if this installer can install the specified resource; otherwise <c>false</c>.</returns>
+        public bool CanInstall(PackageReference package, PackageType type)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException(nameof(package));
+            }
+
+            return package.Scheme.Equals("choco", StringComparison.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Installs the specified resource at the given location.
+        /// </summary>
+        /// <param name="package">The package reference.</param>
+        /// <param name="type">The package type.</param>
+        /// <param name="path">The location where to install the package.</param>
+        /// <returns>The installed files.</returns>
+        public IReadOnlyCollection<IFile> Install(PackageReference package, PackageType type, DirectoryPath path)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException(nameof(package));
+            }
+
+            if (path == null)
+            {
+                throw new ArgumentNullException(nameof(path));
+            }
+
+            path = path.MakeAbsolute(_environment);
+
+            // Install the package.
+            _log.Debug("Installing Chocolatey package {0}...", package.Package);
+            var process = _processRunner.Start("choco", new ProcessSettings
+            {
+                Arguments = GetArguments(package, path),
+                RedirectStandardOutput = true,
+                Silent = _log.Verbosity < Verbosity.Diagnostic
+            });
+
+            process.WaitForExit();
+
+            var exitCode = process.GetExitCode();
+            if (exitCode != 0)
+            {
+                _log.Warning("Chocolatey exited with {0}", exitCode);
+                var output = string.Join(Environment.NewLine, process.GetStandardOutput());
+                _log.Verbose(Verbosity.Diagnostic, "Output:\r\n{0}", output);
+            }
+
+            return new List<IFile>();
+        }
+
+        private static ProcessArgumentBuilder GetArguments(
+            PackageReference definition,
+            DirectoryPath installationRoot)
+        {
+            var arguments = new ProcessArgumentBuilder();
+
+            arguments.Append("install");
+            arguments.AppendQuoted(definition.Package);
+            arguments.Append("-y");
+
+            // if an absolute uri is specified for source, use this
+            // otherwise check config for customise package source/s
+            if (definition.Address != null)
+            {
+                arguments.Append("--source");
+                arguments.AppendQuoted(definition.Address.AbsoluteUri);
+            }
+
+            // Version
+            if (definition.Parameters.ContainsKey("version"))
+            {
+                arguments.Append("--version");
+                arguments.AppendQuoted(definition.Parameters["version"].First());
+            }
+
+            // Prerelease
+            if (definition.Parameters.ContainsKey("prerelease"))
+            {
+                arguments.Append("--prerelease");
+            }
+
+            return arguments;
+        }
+    }
+}
