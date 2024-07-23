b964fbbf1780b49469a28b6635812958d44a75ab
Nils Andresen <nils@nils-andresen.de>
2024-07-24 00:03:49 +0200

    (#22) Bump Cake.Recipe to 3.1.1

--- build.ps1
+++ build.ps1
@@ -1,5 +1,3 @@
-$ErrorActionPreference = 'Stop'
-
 Set-Location -LiteralPath $PSScriptRoot
 
 $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = '1'
@@ -7,9 +5,6 @@ $env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
 $env:DOTNET_NOLOGO = '1'
 
 dotnet tool restore
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
-
-dotnet cake recipe.cake --bootstrap
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
 dotnet cake recipe.cake @args
