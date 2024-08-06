3312dab9920bada5be9c6115b9ca42fbf6846974
GitHub <noreply@github.com>
2024-08-06 06:37:45 +0000

    Continuous Integration Publish: 72785940d2be7e790c8c862715fc5bba64199373 Merge pull request #66 from nils-a/feature/GH-3
    (#3) disable installing on non-windows

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -39,6 +39,20 @@ var searchModule = function() {
     a(
         {
             id:0,
+            title:"IChocolateyContentResolver",
+            content:"IChocolateyContentResolver",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.Chocolatey.Module/api/Cake.Chocolatey.Module/IChocolateyContentResolver',
+            title:"IChocolateyContentResolver",
+            description:""
+        }
+    );
+    a(
+        {
+            id:1,
             title:"ChocolateyPackageInstaller",
             content:"ChocolateyPackageInstaller",
             description:'',
@@ -52,7 +66,7 @@ var searchModule = function() {
     );
     a(
         {
-            id:1,
+            id:2,
             title:"ChocolateyModule",
             content:"ChocolateyModule",
             description:'',
@@ -66,7 +80,7 @@ var searchModule = function() {
     );
     a(
         {
-            id:2,
+            id:3,
             title:"ChocolateyContentResolver",
             content:"ChocolateyContentResolver",
             description:'',
@@ -75,20 +89,6 @@ var searchModule = function() {
         {
             url:'/Cake.Chocolatey.Module/api/Cake.Chocolatey.Module/ChocolateyContentResolver',
             title:"ChocolateyContentResolver",
-            description:""
-        }
-    );
-    a(
-        {
-            id:3,
-            title:"IChocolateyContentResolver",
-            content:"IChocolateyContentResolver",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.Chocolatey.Module/api/Cake.Chocolatey.Module/IChocolateyContentResolver',
-            title:"IChocolateyContentResolver",
             description:""
         }
     );
